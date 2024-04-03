0daa965fdfd79a648af01afca3cac917fd77e401
Kate <popovaekaterina13@yandex.ru>
2024-04-03 15:50:32 +0300

    Final version

--- js/main.js
+++ js/main.js
@@ -27,48 +27,50 @@
 
 //Scroll 
 document.addEventListener("DOMContentLoaded", function () {
-  
-  gsap.registerPlugin(ScrollTrigger);
 
-  let tt = gsap.timeline({
-    scrollTrigger: {
-      trigger: ".presentation__description",
-      start: "top center",
-      end: "bottom center", 
-      scrub: true,
-      toggleActions: "play pause resume reset"
-    }
-  });
+  if (window.matchMedia("(min-width: 900px)").matches) {
+    gsap.registerPlugin(ScrollTrigger);
 
-  tt.fromTo(".presentation__description", {x: -50, opacity: 0.3}, {
-    opacity: 1, x: 0,
-    duration: 1,
-  });
+    let tt = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".presentation__description",
+        start: "top center",
+        end: "bottom center", 
+        scrub: true,
+        toggleActions: "play pause resume reset"
+      }
+    });
 
-  let tl = gsap.timeline({
-    scrollTrigger: {
-      trigger: ".presentation-content",
-      start: "top center",
-      end: "bottom center", 
-      scrub: true,
-      toggleActions: "play pause resume reset"
-    }
-  });
+    tt.fromTo(".presentation__description", {x: -50, opacity: 0.3}, {
+      opacity: 1, x: 0,
+      duration: 1,
+    });
 
-  tl.to("#first", {
-    x: 100,
-    duration: 1,
-  })
-  .to(".presentation-content__text_left", {
-    x: -100,
-    duration: 1,
-  })
-  .to("#last", {
-    x: 100,
-    duration: 1,
-  });
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".presentation-content",
+        start: "top center",
+        end: "bottom center", 
+        scrub: true,
+        toggleActions: "play pause resume reset"
+      }
+    });
 
-  });
+    tl.to("#first", {
+      x: 75,
+      duration: 1,
+    })
+    .to(".presentation-content__text_left", {
+      x: -75,
+      duration: 1,
+    })
+    .to("#last", {
+      x: 75,
+      duration: 1,
+    });
+  }
+});
+
 
 
 
@@ -82,3 +84,26 @@ const swiperAuto = new Swiper(".swiper-auto", {
   }
 
 });
+
+
+//Mobile nav
+
+const mobileNavOpen = document.querySelector("#open-mobile-nav-btn");
+const mobileNavCloseBtn = document.querySelector("#close-mobile-nav-btn");
+const mobileNav = document.querySelector(".mobile-nav");
+const navBottomIcons = document.querySelector(".nav-bottom__icons");
+
+
+mobileNavOpen.addEventListener("click", function() {
+  mobileNav.classList.add("mobile-nav--open");
+  navBottomIcons.classList.add("none");
+
+})
+
+
+mobileNavCloseBtn.addEventListener("click", function() {
+  mobileNav.classList.remove("mobile-nav--open");
+  navBottomIcons.classList.remove("none");
+})
+
+
